22a3c09a2223480091ad15ee85317c88f07f13e3
clem-ant <clementfourmii@hotmail.fr>
2020-10-20 23:09:57 +0200

    Modification de inverse mais ça marche pas au niveau du echo ligne

--- inverse.sh
+++ inverse.sh
@@ -2,14 +2,26 @@
 
 if [ "$#" -ne 1 -a "$#" -ne 2 ]
 then
-	echo "Erreur : $0 nom_fichier1 [nom_fichier2]" >&2
+	echo "Erreur : $0 nom_fichier1 [nom_fichier2]" >&2 
 fi
 
 case $# in
-	1 ) nbligne=`wc -l $1|cut -f1 -d ' ' `
+	1 )	nbligne=`wc -l $1|cut -f1 -d ' '`
 		echo Un seul parametre
-		echo nombre de ligne : $nbligne;;
-	2 ) 
-		cp $1 $2
-		sh $0 $2;;
+		echo nombre de ligne : $nbligne
+		while [ $nbligne -ne 0 ]
+		do
+			if [ "$#" -eq 2 ]
+			then
+				echo test
+				echo ligne $nbligne : `head -n $nbligne|tail -1` >> $2
+			else
+				echo testelse
+				echo ligne $nbligne : `head -n $nbligne|tail -1` >> fichier2
+				echo testelse2
+			fi
+			echo test
+			nbligne=$((nbligne-1))
+		done
+#	2 ) cp $1 $2 && sh $0 $2;;
 esac
