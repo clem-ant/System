43baee4a8ea7f71f4f5240eff51edc8b679b94c5
clem-ant <clementfourmii@hotmail.fr>
2020-10-21 19:53:36 +0200

    Le 16est good

--- exo16.sh
+++ exo16.sh
@@ -9,10 +9,11 @@ relancer_n()
 {
 	for i in $(seq 1 $1);
 	do
-		`tail -n $i temp|head -1`
+		`grep '^[0-9] :' temp|tail -n $i|head -1 | cut -f2 -d ":"`
 	done
 }
 affichage_menu
+index=0
 while read choix var1
 do
 	case $choix in
@@ -27,7 +28,8 @@ do
 			echo vous relancez les $var1 dernieres commandes
 			relancer_n $var1;;
 		* )
-			$choix $var1 && echo $choix $var1 >> temp
+			$choix $var1 && echo $index : $choix $var1  >> temp && $choix $var1 >> temp
 	esac
 	affichage_menu
+	index=$((index+1))
 done
