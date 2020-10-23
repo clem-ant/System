f195457b417f83342e593df3f00a91bbab67e08a
clem-ant <clementfourmii@hotmail.fr>
2020-10-23 10:25:16 +0200

    exo17 validé par le prof, reste l'exo 18 avec une boucle

---
+++ TP5/exo18.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+for dir in `sh exo17.sh $1`
+do 
+	echo $dir
+done
--- TP5/tree.sh
+++ TP5/tree.sh
@@ -3,15 +3,16 @@
 if [ $# -ne 1 ]
 then
 	echo "$0 nom_dossier1 : Il faut au moins un dossier en paramètre" >&2
+	exit 1
 fi
 
 pwd
 ls -R | grep ":$" |   \
    sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
-# 1st sed: remove colons
+# 1er sed: remove colons
 # 2nd sed: replace higher level folder names with dashes
-# 3rd sed: indent graph three spaces
-# 4th sed: replace first dash with a vertical bar
+# 3eme sed: indent graph three spaces
+# 4eme sed: replace first dash with a vertical bar
 if [ `ls -F -1 | grep "/" | wc -l` = 0 ]   # check if no folders
    then echo "   -> no sub-directories"
    fi
